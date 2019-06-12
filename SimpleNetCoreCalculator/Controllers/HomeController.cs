3ee4229d9f96242615629457968f60c023712fd6
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-06-11 21:41:00 -0500

    changed back my controller to the default but simply changed the index methods to what I wanted for the tutorial in case I want to develop further after the tutorial is complete

--- SimpleNetCoreCalculator/Controllers/HomeController.cs
+++ SimpleNetCoreCalculator/Controllers/HomeController.cs
@@ -6,19 +6,40 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SimpleNetCoreCalculator.Models;
 
-public class HomeController : Controller
+namespace SimpleNetCoreCalculator.Controllers
 {
-    [HttpGet]
-    public IActionResult Index()
+    public class HomeController : Controller
     {
-        return View();
-    }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-    [HttpPost]
-    public IActionResult Index(Operation model)
-    {
-        if (model.OperationType == OperationType.Addition)
-            model.Result = model.NumberA + model.NumberB;
-        return View(model);
+        [HttpPost]
+        public IActionResult Index(Operation model)
+        {
+            if (model.OperationType == OperationType.Addition)
+                model.Result = model.NumberA + model.NumberB;
+            return View(model);
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
